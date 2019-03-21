d09b2a71da9c7c3c8727be0102dde8d069d533d5
cshah <chintanjshah@gmail.com>
2019-03-21 08:54:47 -0700

    Change Data Capture

--- src/main/java/com/spring/client/StreamingClient.java
+++ src/main/java/com/spring/client/StreamingClient.java
@@ -12,13 +12,17 @@ import static com.salesforce.emp.connector.LoginHelper.login;
 
 public class StreamingClient {
 
+    public static final String username = "chintanjshah@gmail.com";
+    public static final String password = "Welcome1";
+    public static final String topic = "/data/AccountChangeEvent"; // "/event/Error_Exception_Log__e";
+
     public static void main(String args[]) throws Exception {
         long replayFrom = EmpConnector.REPLAY_FROM_EARLIEST;
-        BayeuxParameters params = login("streaming@springsoa.com", "Welcome1");
+        BayeuxParameters params = login(username, password);
         EmpConnector connector = new EmpConnector(params);
         Consumer<Map<String, Object>> consumer = event -> System.out.println(String.format("Received:\n%s", event));
         connector.start().get(5, TimeUnit.SECONDS);
-        TopicSubscription subscription = connector.subscribe("/event/UpdateObjectEvent__e", replayFrom, consumer ).get(5, TimeUnit.SECONDS);
+        TopicSubscription subscription = connector.subscribe(topic, replayFrom, consumer ).get(5, TimeUnit.SECONDS);
         System.out.println(String.format("Subscribed: %s", subscription));
         //subscription.cancel();
         //connector.stop();
